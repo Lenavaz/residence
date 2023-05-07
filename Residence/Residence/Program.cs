1916251eb8b9a74f5f0d1b3342544f00582cbf56
Lenavaz <lenavaz.pt@gmail.com>
2023-05-07 17:52:22 +0100

    first commit

---
+++ Residence/Residence/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Residence
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Student[] vect = new Student[10];
+
+            Console.Write("How many rooms will be rented (1 to 10): ");
+            int n = int.Parse(Console.ReadLine());
+
+
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Rent #{i}:");
+                Console.Write("Enter name: ");
+                string name = Console.ReadLine();
+                Console.Write("Enter email: ");
+                string email = Console.ReadLine();
+                Console.Write("Room number: ");
+                int room = int.Parse(Console.ReadLine());
+                vect[room] = new Student(name, email);
+
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Rented Rooms: ");
+            for (int i = 0; i < 10; i++)
+            {
+                if (vect[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vect[i]);
+                }
+            }
+
+        }
+    }
+}
